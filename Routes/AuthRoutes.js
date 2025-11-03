06fb7d1849e1369ec24e260ebcbbe88b86b7c47a
vivek-cmtai <vivek.joshi@completrix.com>
2025-11-03 22:12:21 +0530

    File name fixed

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -2,8 +2,9 @@ import express from "express";
 const router = express.Router();
 import { registerUser, loginUser,currentOwner } from "../Controllers/AuthController.js";
 import { verifyToken } from "../Middlewares/middleware.js";
-import wrapAsync from "../Middlewares/wrapAsync.js";
+import wrapAsync from "../Middlewares/WrapAsync.js";
 router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.get("/me",verifyToken, wrapAsync(currentOwner))
+
 export default router
